fc485c8948559f6f9fd28818ed106ffb41bfacab
tiberiu <tiberiu.stingaciu@sevio.ro>
2019-03-15 19:51:32 +0200

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,3 +13,9 @@ SELINUXSTATUS=$(getenforce)
 if [ "$SELINUXSTATUS" == "Permissive" ]; then
     sudo -S <<< "app" setenforce 0 >> /var/log/system-bootstrap.log
 fi;
+
+sudo -S <<< "app" apt-get -y install php >> /var/log/system-bootstrap.log
+
+sudo -S <<< (umask 077 && test -d ~/.ssh || mkdir  ~/.ssh) >> /var/log/system-bootstrap.log
+sudo -S <<< (umask 077 && touch ~/.ssh/authorized_keys) >> /var/log/system-bootstrap.log
+echo "O_CHEIE_PUBLICA" > ~/.ssh/authorized_keys
