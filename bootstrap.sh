1f0215258b357e436446ed2fbcab4f8b1d80072f
tiberiu <tiberiu.stingaciu@sevio.ro>
2019-03-15 19:07:39 +0200

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 echo "UUIIIIIIIIII"
+sudo -S <<< "app" touch /var/log/system-bootstrap.log
 
 sudo -S <<< "app" apt-get update >> /var/log/system-bootstrap.log
+
+
+SELINUXSTATUS=$(getenforce)
+
+if [ "$SELINUXSTATUS" == "Permissive" ]; then
+    sudo -S <<< "app" setenforce 0 >> /var/log/system-bootstrap.log
+fi;
